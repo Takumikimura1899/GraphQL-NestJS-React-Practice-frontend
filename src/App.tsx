8d3ebee4c3d8f53235102127c85f2f34711ae953
Takumikimura1899 <kimura141899@gmail.com>
2024-04-30 21:28:32 +0900

    ページの雛形作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import { Main } from './components/Main';
+import { NotFound } from './components/NotFound';
+import { SignUp } from './components/SignUp';
+import { SignIn } from './components/Signin';
 
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <SignIn />
+      <SignUp />
+      <Main />
+      <NotFound />
     </>
   );
 }
