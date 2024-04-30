d40db93591a007ef065ac6c153f9892d14ee3f40
Takumikimura1899 <kimura141899@gmail.com>
2024-04-30 21:31:21 +0900

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Main } from './components/Main';
 import { NotFound } from './components/NotFound';
 import { SignUp } from './components/SignUp';
 import { SignIn } from './components/Signin';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <SignIn />
-      <SignUp />
-      <Main />
-      <NotFound />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/' element={<Main />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
