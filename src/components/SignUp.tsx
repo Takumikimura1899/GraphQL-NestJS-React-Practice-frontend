2da824cd297db09fc713a0c85f9625c830f03fed
Takumikimura1899 <kimura141899@gmail.com>
2024-05-02 21:04:23 +0900

    サインアップ機能実装

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -11,6 +11,11 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { SIGN_IN, SIGN_UP } from '../mutations/authMutations';
+import { User } from '../types/user';
+import { SignInResponse } from '../types/signinResponse';
+import { useNavigate } from 'react-router-dom';
 
 const theme = createTheme();
 
@@ -18,10 +23,37 @@ export const SignUp = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signUp] = useMutation<{ createUser: User }>(SIGN_UP);
+  const [signIn] = useMutation<SignInResponse>(SIGN_IN);
+  const navigate = useNavigate();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(name, email, password);
+    const signUpInput = { name, email, password };
+
+    try {
+      const result = await signUp({
+        variables: {
+          createUserInput: signUpInput,
+        },
+      });
+      if (result.data?.createUser) {
+        const signInInput = { email, password };
+        const result = await signIn({
+          variables: {
+            signInInput,
+          },
+        });
+        if (result.data) {
+          localStorage.setItem('token', result.data.signIn.accessToken);
+        }
+        localStorage.getItem('token') && navigate('/');
+      }
+    } catch (error: any) {
+      console.log(error.message);
+      alert('ユーザーの作成に失敗しました');
+      return;
+    }
   };
 
   return (
